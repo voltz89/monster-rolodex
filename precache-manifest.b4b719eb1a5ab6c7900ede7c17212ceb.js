7187c9eefd1b9809833c13c835778f52bafe363c
voltz89 <zaft_xz000-nu@hotmail.com>
2019-12-01 22:53:52 +0700

    Updates

---
+++ precache-manifest.b4b719eb1a5ab6c7900ede7c17212ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed0257a8e822b433830a967e58f72ab",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c3a6b26ecb2ebf09e84b",
+    "url": "/monster-rolodex/static/css/main.3320b1b6.chunk.css"
+  },
+  {
+    "revision": "3f3b3579c3704ef15547",
+    "url": "/monster-rolodex/static/js/2.bcb9ac26.chunk.js"
+  },
+  {
+    "revision": "c3a6b26ecb2ebf09e84b",
+    "url": "/monster-rolodex/static/js/main.6bf58f15.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monster-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
